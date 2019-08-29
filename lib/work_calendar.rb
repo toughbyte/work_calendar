fa452bbdafe3ced18328f8a436704c3d79286981
Сергей Томашевский <sergey.tomashevsky@toughbyte.com>
2019-08-29 19:36:44 +0300

    Init gem

---
+++ lib/work_calendar.rb
@@ -0,0 +1,6 @@
+require "work_calendar/version"
+
+module WorkCalendar
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/work_calendar/version.rb
@@ -0,0 +1,3 @@
+module WorkCalendar
+  VERSION = "0.1.0"
+end
