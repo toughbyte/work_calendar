33cee0bf864f66032f640166084fe4093a928c5a
Сергей Томашевский <sergey.tomashevsky@toughbyte.com>
2019-08-30 11:57:15 +0300

    Rubocop now works with test

--- test/rubocop_test.rb
+++ test/rubocop_test.rb
@@ -3,15 +3,16 @@
 require 'test_helper'
 
 class RubocopTest < Minitest::Test
-  #   def initialize(_name)
-  #     modified = `git diff master --name-only`
-  #     untracked = `git ls-files --others --exclude-standard`
-  #     excluded = Dir.glob('bin/*')
-  #     files = (modified.split("\n") + untracked.split("\n") - excluded).join(' ')
-  #     @report = files.blank? ? '' : `bundle exec rubocop #{files}`
-  #   end
+  def initialize(name)
+    super(name)
+    modified = `git diff master --name-only`
+    untracked = `git ls-files --others --exclude-standard`
+    excluded = Dir.glob('bin/*')
+    files = (modified.split("\n") + untracked.split("\n") - excluded).join(' ')
+    @report = files.blank? ? '' : `bundle exec rubocop #{files} -a`
+  end
 
-  #   def test_with_rubocop
-  #     assert_no_match(/Offenses:/, @report, "Rubocop report:\n#{@report}")
-  #   end
+  def test_with_rubocop
+    refute_match(/Offenses:/, @report, "Rubocop report:\n#{@report}")
+  end
 end
